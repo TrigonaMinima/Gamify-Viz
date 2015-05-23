1d1532f124e74b7401e4ef7006b077da28b1b4a0
TrigonaMinima <shivamrana95@yahoo.in>
2015-05-24 03:24:04 +0530

    Movies' related visualizations

---
+++ movies.R
@@ -0,0 +1,19 @@
+library(ggplot2)
+
+mov = read.csv("Data/movies.csv")
+
+
+# > str(mov)
+# 'data.frame':   224 obs. of  6 variables:
+#  $ Movies : Factor w/ 224 levels "12 Angry Men ",..: 180 181 187 189 188 49 87 81 75 82 ...
+#  $ Lang   : Factor w/ 11 levels "Eng","French (Eng subs)",..: 1 1 1 1 1 2 1 1 1 1 ...
+#  $ Ratings: int  4 3 5 5 5 3 3 5 5 5 ...
+#  $ Year   : int  2012 2013 2001 2002 2003 2003 2011 2001 2002 2004 ...
+#  $ Count  : int  2 1 1 1 2 1 1 2 4 3 ...
+#  $ Type   : Factor w/ 4 levels "","Animation",..: 1 1 1 1 1 1 1 1 1 1 ...
+
+
+
+
+
+ggplot(mov, aes(x=Year, y=Lang)) + geom_point()
